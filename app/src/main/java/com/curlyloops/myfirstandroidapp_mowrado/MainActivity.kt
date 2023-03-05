6f08cbd8dcc63b6d4bd0f3ee0dbcc6eea76aad08
KHS <hyoseo118@gmail.com>
2023-03-05 17:12:25 +0900

    Initial commit

---
+++ app/src/main/java/com/curlyloops/myfirstandroidapp_mowrado/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.curlyloops.myfirstandroidapp_mowrado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
